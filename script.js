21d4122d7dcad4f49ed1cdeb230345f9fde47e7d
Gabriel Crosetti <crosetti.gabriel@gmail.com>
2021-09-29 00:50:37 -0400

    Added event listeners to js to trigger timer

--- script.js
+++ script.js
@@ -2,18 +2,15 @@
 var score = 0;
 var questionsIndex = 0;
 
-var timer = document.querySelector("#start");
+var start = document.querySelector("#start");
 var currentTime = document.querySelector("#currentTime");
-var questionsDiv = document.querySelector("#questionBox");
+var questionBox = document.querySelector("#questionBox");
 var container = document.querySelector("#container");
 
 
 var timeLeft = 60
-
 var holdInterval = 0
-
 var penaltyTime = 7;
-
 var ulCreate = document.createElement("ul");
 
 
@@ -43,7 +40,45 @@ var quizQuestions = [ {
         answer: "||!"
     } ]
 
+// Triggers timer on button, shows user a display on the screen
+timer.addEventListener("click", function () {
+    // We are checking zero because its originally set to zero
+    if (holdInterval === 0) {
+        holdInterval = setInterval(function () {
+            timeLeft--;
+            currentTime.textContent = "Time: " + timeLeft;
 
+            if (timeLeft <= 0) {
+                clearInterval(holdInterval);
+                allDone();
+                currentTime.textContent = "Time's up!";
+            }
+        }, 1000);
+    }
+    render(questionIndex);
+});
+
+// Renders questions and choices to page: 
+function render(questionIndex) {
+    // Clears existing data 
+    questionsDiv.innerHTML = "";
+    ulCreate.innerHTML = "";
+    // For loops to loop through all info in array
+    for (var i = 0; i < questions.length; i++) {
+        // Appends question title only
+        var userQuestion = questions[questionIndex].title;
+        var userChoices = questions[questionIndex].choices;
+        questionsDiv.textContent = userQuestion;
+    }
+    // New for each for question choices
+    userChoices.forEach(function (newItem) {
+        var listItem = document.createElement("li");
+        listItem.textContent = newItem;
+        questionsDiv.appendChild(ulCreate);
+        ulCreate.appendChild(listItem);
+        listItem.addEventListener("click", (compare));
+    })
+}
 
 
 
@@ -69,31 +104,26 @@ var quizQuestions = [ {
 // Variables and js code for leaderboard
 
     var highScore = document.querySelector("#highScore");
-var clear = document.querySelector("#clear");
-var goBack = document.querySelector("#goBack");
+var clear = document.querySelector("#clearScores");
+var goBack = document.querySelector("#return");
 
 // Added an event listener to clear past scores 
-
 clear.addEventListener("click", function () {
     localStorage.clear();
     location.reload();
 });
-
-// Retreives local stroage 
+// Retreive from local storage
 var allScores = localStorage.getItem("allScores");
 allScores = JSON.parse(allScores);
 
 if (allScores !== null) {
-
     for (var i = 0; i < allScores.length; i++) {
-
         var createLi = document.createElement("li");
         createLi.textContent = allScores[i].initials + " " + allScores[i].score;
         highScore.appendChild(createLi);
-
     }
 }
 // Event listener to move to index page
 goBack.addEventListener("click", function () {
-    window.location.replace("./index.html");
+    window.location.replace("index.html");
 });
