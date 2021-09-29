e6786a11476c5d8a0be79c6d5c965b98a229a17c
Gabriel Crosetti <crosetti.gabriel@gmail.com>
2021-09-29 00:34:39 -0400

    First changes to leaderboard html and added first js for leaderboard

--- script.js
+++ script.js
@@ -42,3 +42,58 @@ var quizQuestions = [ {
         choices: ["+=", "||", "*=", "="],
         answer: "||!"
     } ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Variables and js code for leaderboard
+
+    var highScore = document.querySelector("#highScore");
+var clear = document.querySelector("#clear");
+var goBack = document.querySelector("#goBack");
+
+// Added an event listener to clear past scores 
+
+clear.addEventListener("click", function () {
+    localStorage.clear();
+    location.reload();
+});
+
+// Retreives local stroage 
+var allScores = localStorage.getItem("allScores");
+allScores = JSON.parse(allScores);
+
+if (allScores !== null) {
+
+    for (var i = 0; i < allScores.length; i++) {
+
+        var createLi = document.createElement("li");
+        createLi.textContent = allScores[i].initials + " " + allScores[i].score;
+        highScore.appendChild(createLi);
+
+    }
+}
+// Event listener to move to index page
+goBack.addEventListener("click", function () {
+    window.location.replace("./index.html");
+});
